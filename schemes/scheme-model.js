ff9c2b93e021bd2632687d2f89e01b8a43901dfd
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-11-06 15:48:47 +0100

    created functions for my endpoints

---
+++ schemes/scheme-model.js
@@ -0,0 +1,36 @@
+const db = require('../data/db-config');
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    update,
+    remove,
+}
+
+function find() {
+  return db('schemes')
+}
+  
+function findById(id) {
+    return db('schemes')
+    .where({ id })
+    .first();
+}
+
+return db("schemes as sc")
+    .join("steps as s", "s.scheme_id", "sc.id")
+    .select("s.id", "s.step_number", "s.instructions", "sc.scheme_name")
+    .where("s.scheme_id", id);
+
+function update(changes, id) {
+    return db('schemes')
+    .where({ id })
+    .update(changes);
+}
+
+function remove(id) {
+    return db('schemes')
+    .where('id', id)
+    .del();
+}
